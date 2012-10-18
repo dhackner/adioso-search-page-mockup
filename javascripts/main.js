0bb5f04ccb590b845f7fbddecb4b91c7b4894320
Dan Hackner <dan.hackner@gmail.com>
2012-10-18 13:17:07 -0700

    Whitespace cleanup

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 
-	$(function() {
-    	var state = true;
-    	$( "div.itinerary-box" ).click(function() {
-        	if (!$(this).hasClass('selected')) {
-				$("div.itinerary-box.selected").removeClass( 'selected', 50);
-            	$(this).addClass('selected', 50);
-        	} else {
-            	$(this).switchClass('selected', 'hovering', 50);
-        	}
-        	state = !state;
-    	});
+  $(function() {
+      var state = true;
+      $( "div.itinerary-box" ).click(function() {
+          if (!$(this).hasClass('selected')) {
+        $("div.itinerary-box.selected").removeClass( 'selected', 50);
+              $(this).addClass('selected', 50);
+          } else {
+              $(this).switchClass('selected', 'hovering', 50);
+          }
+          state = !state;
+      });
 
-    	$('div.itinerary-box').hover(function() {
-        	$(this).addClass('hovering');
-    	}, function() {
-        	$(this).removeClass('hovering');
-    	});
-	});
+      $('div.itinerary-box').hover(function() {
+          $(this).addClass('hovering');
+      }, function() {
+          $(this).removeClass('hovering');
+      });
+  });
 });
